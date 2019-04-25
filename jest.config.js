d3ae800cb4073db2524f6b76b954b71ee672b322
paul <drawmindmap@gmail.com>
2019-04-25 12:31:59 +0800

    Added unit test

--- jest.config.js
+++ jest.config.js
@@ -8,4 +8,5 @@ module.exports = {
   collectCoverageFrom: [
     'src/**',
   ],
+  testEnvironment: 'jest-environment-jsdom-fourteen',
 };
--- test/Util.test.js
+++ test/Util.test.js
@@ -7,6 +7,7 @@ describe('Util', () => {
     const color = getColorValue('rgba(255, 0, 0, 0.5)');
     expect(color.r).toBe(255);
     expect(color.g).toBe(0);
-    expect(color.b).toBe(127);
+    expect(color.b).toBe(0);
+    expect(color.a).toBe(127);
   });
 });
